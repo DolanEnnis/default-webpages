ed36fdcd720c8a7154d71b4954de52e5f506972a
DolanEnnis <dolan.ennis@gmail.com>
2024-09-28 22:29:19 +0100

    Made Header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'default-webpages';
+export class AppComponent implements OnInit {
+  authService = inject(AuthService)
+  http = inject(HttpClient);
+
+
+ngOnInit(): void {
+  this.authService.user$.subscribe((user: { email: string; displayName: string; }) =>{
+    if (user) {
+      this.authService.currentUserSig.set({
+        email:user.email!,
+        username: user.displayName!,
+      });
+    } else{
+      this.authService.currentUserSig.set(null);
+    }
+  } );
+}
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+      provideRouter(routes), 
+      provideHttpClient(),
+      provideFirebaseApp(() => initializeApp({"projectId":"default-webpages",
+                                              "appId":"1:666481102309:web:5c833fe1a07def7078b30c",
+                                              "storageBucket":"default-webpages.appspot.com",
+                                              "apiKey":"AIzaSyBYHkCfWHPIb4B9hANnkzq47Q8m7X45suE",
+                                              "authDomain":"default-webpages.firebaseapp.com",
+                                              "messagingSenderId":"666481102309",
+                                              "measurementId":"G-ZS533FSR01"})), 
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore())]
 };
